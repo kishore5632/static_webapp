7c86e6ceef4e76975696e7f0c91d9f199d6b854d
kishore5632 <elkishore5@gmail.com>
2025-02-07 12:21:19 +0000

    static web app deployment using k8s

---
+++ terraform/aks.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aks-cluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
